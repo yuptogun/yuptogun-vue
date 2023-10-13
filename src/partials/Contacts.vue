a24d1096e5830cfe5362a9d5bba513960da66cb1
엽토군 <eojin1211@hanmail.net>
2023-10-13 23:07:36 +0900

    misc tweaks

--- src/partials/Contacts.vue
+++ src/partials/Contacts.vue
@@ -27,25 +27,25 @@ const searchPlaceholder = computed(() => {
 <template>
     <div id="contacts" class="p-5">
         <div class="block mb-3">
-            <input type="text" v-model="filterText" class="inline-block border border-gray-300 rounded me-4" :placeholder="searchPlaceholder" />
+            <input type="text" v-model="filterText" class="inline-block border border-gray-300 rounded me-4 mb-2" :placeholder="searchPlaceholder" />
             <div class="inline-block">
-                <label class="me-4 inline-block">
+                <label class="me-4 mb-2 inline-block">
                     <input type="checkbox" class="rounded" v-model="filterTypes" value="socialnetwork" />
                     SNS
                 </label>
-                <label class="me-4 inline-block">
+                <label class="me-4 mb-2 inline-block">
                     <input type="checkbox" class="rounded" v-model="filterTypes" value="content" />
                     창작
                 </label>
-                <label class="me-4 inline-block">
+                <label class="me-4 mb-2 inline-block">
                     <input type="checkbox" class="rounded" v-model="filterTypes" value="dev" />
                     개발
                 </label>
-                <label class="me-4 inline-block">
+                <label class="me-4 mb-2 inline-block">
                     <input type="checkbox" class="rounded" v-model="filterTypes" value="financial" />
                     (금전)거래
                 </label>
-                <label class="me-4 inline-block">
+                <label class="me-4 mb-2 inline-block">
                     <input type="checkbox" class="rounded" v-model="filterTypes" value="contact" />
                     연락처
                 </label>
