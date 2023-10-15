51f6b5d2776b51226bec60670926ca6692602f9c
엽토군 <eojin1211@hanmail.net>
2023-10-15 17:06:01 +0900

    Filtering: case insensitive, alias Fixes #5

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script setup>
 import Intro from './partials/Intro.vue';
 import Contacts from './partials/Contacts.vue';
+import Footer from './partials/Footer.vue';
 </script>
 
 <template>
   <div class="flex flex-row h-full">
     <div class="flex-none p-5 self-start sticky top-0 self-start">
-      <img src="https://gravatar.com/avatar/6827b8f452e98fcda676789477b05de3658e7f81f69d9dc66957eee8cd223c84.jpg?s=160" class="w-12 md:w-24 h-auto rounded-full">
+      <a href="#">
+        <img src="https://gravatar.com/avatar/6827b8f452e98fcda676789477b05de3658e7f81f69d9dc66957eee8cd223c84.jpg?s=160" class="w-16 md:w-32 lg:w-48 h-auto rounded-full">
+      </a>
     </div>
     <div class="pt-5">
       <Intro></Intro>
       <Contacts></Contacts>
     </div>
   </div>
+  <Footer></Footer>
 </template>
 
 <style scoped>
